6c9b26dce64c7334af2f63d279056a85217ddfa0
leandro8080 <leandro.schultze@ict.csbe.ch>
2024-11-08 18:19:57 +0100

    A01 Broken Access Control
    Das Login wurde so verändert, dass es jetzt einen Token zurückgibt. Allerdings wurde die Abfrage noch nicht verändert

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const usernameInput = document.getElementById("username");
-  const passwordInput = document.getElementById("password");
-  const loginButton = document.getElementById("login");
-  const errorText = document.getElementById("error");
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const loginButton = document.getElementById("login");
+    const errorText = document.getElementById("error");
 
-  loginButton.addEventListener("click", async () => {
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
+    loginButton.addEventListener("click", async () => {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const result = await response.text();
+        if (response.status === 200) {
+            localStorage.setItem("token", result);
+            window.location.href = "/";
+        } else {
+            errorText.innerText = result;
+        }
     });
-    const data = await response.json();
-    if (data?.username) {
-      localStorage.setItem("user", JSON.stringify(data));
-      window.location.href = "/";
-    } else {
-      errorText.innerText = data;
-    }
-  });
 });
--- server/api.js
+++ server/api.js
@@ -1,34 +1,46 @@
 const { initializeDatabase, queryDB, insertDB } = require("./database");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 let db;
+const secretKey = process.env.SECRETKEY;
 
 const initializeAPI = async (app) => {
-  db = await initializeDatabase();
-  app.get("/api/feed", getFeed);
-  app.post("/api/feed", postTweet);
-  app.post("/api/login", login);
+    db = await initializeDatabase();
+    app.get("/api/feed", getFeed);
+    app.post("/api/feed", postTweet);
+    app.post("/api/login", login);
 };
 
 const getFeed = async (req, res) => {
-  const query = req.query.q;
-  const tweets = await queryDB(db, query);
-  res.json(tweets);
+    const query = req.query.q;
+    const tweets = await queryDB(db, query);
+    res.json(tweets);
 };
 
 const postTweet = (req, res) => {
-  insertDB(db, req.body.query);
-  res.json({ status: "ok" });
+    insertDB(db, req.body.query);
+    res.json({ status: "ok" });
 };
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
-  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-  const user = await queryDB(db, query);
-  if (user.length === 1) {
-    res.json(user[0]);
-  } else {
-    res.json(null);
-  }
+    const { username, password } = req.body;
+    const query = `SELECT password FROM users WHERE username = "${username}";`;
+    const user = await queryDB(db, query);
+    if (user.length === 1) {
+        if (user[0].password === password) {
+            const token = jwt.sign(
+                {
+                    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+                    data: { username },
+                },
+                secretKey
+            );
+            res.status(200).send(token);
+        }
+    } else {
+        res.status(401).send("Username or password wrong");
+    }
 };
 
 module.exports = { initializeAPI };
