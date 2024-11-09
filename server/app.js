996f0af94d970e84e9466459f9689ce5f0f063ba
leandro8080 <leandro.schultze@ict.csbe.ch>
2024-11-09 15:57:33 +0100

    A05 Security Misconfiguration:
    Das Packet "Helmet" wurde hinzugefügt, da dieses dabei hilft sichere response Headers zu nutzen. Beispielsweise wird das Framework entfernt.

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const http = require("http");
+const helmet = require("helmet");
 const { initializeAPI } = require("./api");
 
 // Create the express server
 const app = express();
 app.use(express.json());
+
+app.use(helmet());
 const server = http.createServer(app);
 
 // deliver static files from the client folder like css, js, images
 app.use(express.static("client"));
 // route for the homepage
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/client/index.html");
+    res.sendFile(__dirname + "/client/index.html");
 });
 
 // Initialize the REST api
@@ -20,5 +23,5 @@ initializeAPI(app);
 //start the web server
 const serverPort = process.env.PORT || 3000;
 server.listen(serverPort, () => {
-  console.log(`Express Server started on port ${serverPort}`);
+    console.log(`Express Server started on port ${serverPort}`);
 });
