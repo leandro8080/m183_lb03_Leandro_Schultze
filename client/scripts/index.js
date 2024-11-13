0fe6adf9dfca317f3f22f6fe401dc446f95e8e65
leandro8080 <leandro.schultze@ict.csbe.ch>
2024-11-13 16:58:59 +0100

    Kleine Bugfixes
    - Im Log wird bei einem Post nun der Text gespeichert statt den Titel, da es diesen nicht gibt.
    
    - Beim Logout wird nun der Token entfernt

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const newTweetInput = document.getElementById("new-tweet");
-    const postTweetButton = document.getElementById("post-tweet");
-    const logoutButton = document.getElementById("logout");
+	const newTweetInput = document.getElementById("new-tweet");
+	const postTweetButton = document.getElementById("post-tweet");
+	const logoutButton = document.getElementById("logout");
 
-    const token = localStorage.getItem("token");
+	const token = localStorage.getItem("token");
 
-    const verifyToken = async () => {
-        const response = await fetch("api/verify-token", {
-            headers: { Authorization: `Bearer ${token}` },
-        });
-        const result = await response.text();
-        console.log(result);
-        if (result !== 200) {
-            window.location.href = "/login.html";
-        }
-    };
+	const verifyToken = async () => {
+		const response = await fetch("api/verify-token", {
+			headers: { Authorization: `Bearer ${token}` }
+		});
+		const result = await response.text();
+		console.log(result);
+		if (result !== 200) {
+			window.location.href = "/login.html";
+		}
+	};
 
-    verifyToken();
+	verifyToken();
 
-    const generateTweet = (tweet) => {
-        const date = new Date(tweet.timestamp).toLocaleDateString("de-CH", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-        });
-        const tweetElement = `
+	const generateTweet = (tweet) => {
+		const date = new Date(tweet.timestamp).toLocaleDateString("de-CH", {
+			hour: "numeric",
+			minute: "numeric",
+			second: "numeric"
+		});
+		const tweetElement = `
         <div id="feed" class="flex flex-col gap-2 w-full">
             <div class="bg-slate-600 rounded p-4 flex gap-4 items-center border-l-4 border-blue-400" >
                 <img src="./img/tweet.png" alt="SwitzerChees" class="w-14 h-14 rounded-full" />
@@ -40,42 +40,44 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         </div>
       `;
-        return tweetElement;
-    };
+		return tweetElement;
+	};
 
-    const getFeed = async () => {
-        const response = await fetch(`/api/feed`, { headers: { Authorization: `Bearer ${token}` } });
-        const tweets = await response.json();
-        const tweetsHTML = tweets.map(generateTweet).join("");
-        document.getElementById("feed").innerHTML = tweetsHTML;
-    };
+	const getFeed = async () => {
+		const response = await fetch(`/api/feed`, {
+			headers: { Authorization: `Bearer ${token}` }
+		});
+		const tweets = await response.json();
+		const tweetsHTML = tweets.map(generateTweet).join("");
+		document.getElementById("feed").innerHTML = tweetsHTML;
+	};
 
-    const postTweet = async () => {
-        const timestamp = new Date().toISOString();
-        const text = newTweetInput.value;
-        await fetch("/api/feed", {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ timestamp, text }),
-        });
-        await getFeed();
-        newTweetInput.value = "";
-    };
+	const postTweet = async () => {
+		const timestamp = new Date().toISOString();
+		const text = newTweetInput.value;
+		await fetch("/api/feed", {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ timestamp, text })
+		});
+		await getFeed();
+		newTweetInput.value = "";
+	};
 
-    postTweetButton.addEventListener("click", postTweet);
-    newTweetInput.addEventListener("keyup", (event) => {
-        if (event.key === "Enter") {
-            postTweet();
-        }
-    });
+	postTweetButton.addEventListener("click", postTweet);
+	newTweetInput.addEventListener("keyup", (event) => {
+		if (event.key === "Enter") {
+			postTweet();
+		}
+	});
 
-    logoutButton.addEventListener("click", () => {
-        localStorage.removeItem("user");
-        window.location.href = "/login.html";
-    });
+	logoutButton.addEventListener("click", () => {
+		localStorage.removeItem("token");
+		window.location.href = "/login.html";
+	});
 
-    getFeed();
+	getFeed();
 });
--- server/api.js
+++ server/api.js
@@ -11,123 +11,129 @@ aes.setSecretKey(process.env.AESSECRETKEY);
 const secretKey = process.env.SECRETKEY;
 
 const initializeAPI = async (app) => {
-    db = await initializeDatabase();
-    app.get("/api/feed", getFeed);
-    app.post("/api/feed", body("username").escape(), body("timestamp").escape(), body("text").notEmpty().withMessage("Text is empty").escape(), postTweet);
-    app.post("/api/login", body("username").escape(), login);
-    app.get("/api/verify-token", verifyToken);
+	db = await initializeDatabase();
+	app.get("/api/feed", getFeed);
+	app.post(
+		"/api/feed",
+		body("username").escape(),
+		body("timestamp").escape(),
+		body("text").notEmpty().withMessage("Text is empty").escape(),
+		postTweet
+	);
+	app.post("/api/login", body("username").escape(), login);
+	app.get("/api/verify-token", verifyToken);
 };
 
 const getFeed = async (req, res) => {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-        req.log.warn("Unauthorized access attempt to feeds");
-        return res.sendStatus(401);
-    }
-    const token = authHeader.split(" ")[1];
-    if (!token) {
-        req.log.warn("Unauthorized access attempt to feeds");
-        return res.sendStatus(401);
-    }
-    jwt.verify(token, secretKey, async (err) => {
-        if (err) {
-            req.log.warn("Token verification failed when accessing feeds");
-            return res.sendStatus(401);
-        }
-        const query = "SELECT * FROM tweets ORDER BY id DESC";
-        const tweets = await queryDB(db, query);
-        for (i = 0; i < tweets.length; i++) {
-            tweets[i].username = aes.decrypt(tweets[i].username);
-            tweets[i].timestamp = aes.decrypt(tweets[i].timestamp);
-            tweets[i].text = aes.decrypt(tweets[i].text);
-        }
-        req.log.info("Tweets retrieved successfully");
-        res.status(200).json(tweets);
-    });
+	const authHeader = req.headers["authorization"];
+	if (!authHeader) {
+		req.log.warn("Unauthorized access attempt to feeds");
+		return res.sendStatus(401);
+	}
+	const token = authHeader.split(" ")[1];
+	if (!token) {
+		req.log.warn("Unauthorized access attempt to feeds");
+		return res.sendStatus(401);
+	}
+	jwt.verify(token, secretKey, async (err) => {
+		if (err) {
+			req.log.warn("Token verification failed when accessing feeds");
+			return res.sendStatus(401);
+		}
+		const query = "SELECT * FROM tweets ORDER BY id DESC";
+		const tweets = await queryDB(db, query);
+		for (i = 0; i < tweets.length; i++) {
+			tweets[i].username = aes.decrypt(tweets[i].username);
+			tweets[i].timestamp = aes.decrypt(tweets[i].timestamp);
+			tweets[i].text = aes.decrypt(tweets[i].text);
+		}
+		req.log.info("Tweets retrieved successfully");
+		res.status(200).json(tweets);
+	});
 };
 
 const postTweet = (req, res) => {
-    const result = validationResult(req);
-    if (result.errors.length > 0) {
-        req.log.info("Post validation failed when posting tweet");
-        return res.sendStatus(400);
-    }
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-        req.log.warn("Unauthorized attempt to post tweet");
-        return res.sendStatus(401);
-    }
-    const token = authHeader.split(" ")[1];
-    if (!token) {
-        req.log.warn("Unauthorized attempt to post tweet");
-        return res.sendStatus(401);
-    }
-    jwt.verify(token, secretKey, async (err, decoded) => {
-        if (err) {
-            req.log.warn("Token verification failed when accessing feeds");
-            return res.sendStatus(401);
-        }
+	const result = validationResult(req);
+	if (result.errors.length > 0) {
+		req.log.info("Post validation failed when posting tweet");
+		return res.sendStatus(400);
+	}
+	const authHeader = req.headers["authorization"];
+	if (!authHeader) {
+		req.log.warn("Unauthorized attempt to post tweet");
+		return res.sendStatus(401);
+	}
+	const token = authHeader.split(" ")[1];
+	if (!token) {
+		req.log.warn("Unauthorized attempt to post tweet");
+		return res.sendStatus(401);
+	}
+	jwt.verify(token, secretKey, async (err, decoded) => {
+		if (err) {
+			req.log.warn("Token verification failed when accessing feeds");
+			return res.sendStatus(401);
+		}
 
-        const { username } = decoded.data;
-        const { timestamp, text } = req.body;
+		const { username } = decoded.data;
+		const { timestamp, text } = req.body;
 
-        const encryptedUsername = aes.encrypt(username);
-        const encryptedTimestamp = aes.encrypt(timestamp);
-        const encryptedText = aes.encrypt(text);
-        const query = `INSERT INTO tweets (username, timestamp, text) VALUES ('${encryptedUsername}', '${encryptedTimestamp}', '${encryptedText}')`;
-        insertDB(db, query);
-        req.log.info(`Post created successfully with title: "${title}"`);
-        res.json({ status: "ok" });
-    });
+		const encryptedUsername = aes.encrypt(username);
+		const encryptedTimestamp = aes.encrypt(timestamp);
+		const encryptedText = aes.encrypt(text);
+		const query = `INSERT INTO tweets (username, timestamp, text) VALUES ('${encryptedUsername}', '${encryptedTimestamp}', '${encryptedText}')`;
+		insertDB(db, query);
+		req.log.info(`Post created successfully with text: "${text}"`);
+		res.json({ status: "ok" });
+	});
 };
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
-    const query = `SELECT password FROM users WHERE username = "${username}";`;
-    const user = await queryDB(db, query);
-    if (user.length === 1) {
-        const hashedPassword = user[0].password;
-        const match = await bcrypt.compare(password, hashedPassword);
-        if (match) {
-            const token = jwt.sign(
-                {
-                    exp: Math.floor(Date.now() / 1000) + 60 * 60,
-                    data: { username },
-                },
-                secretKey
-            );
-            req.log.info(`User logged in successfully`);
-            res.status(200).send(token);
-        } else {
-            req.log.warn(`Login failed: incorrect password`);
-            res.status(401).send("Username or password wrong");
-        }
-    } else {
-        req.log.warn(`Login failed: didn't find username: ${username}`);
-        res.status(401).send("Username or password wrong");
-    }
+	const { username, password } = req.body;
+	const query = `SELECT password FROM users WHERE username = "${username}";`;
+	const user = await queryDB(db, query);
+	if (user.length === 1) {
+		const hashedPassword = user[0].password;
+		const match = await bcrypt.compare(password, hashedPassword);
+		if (match) {
+			const token = jwt.sign(
+				{
+					exp: Math.floor(Date.now() / 1000) + 60 * 60,
+					data: { username }
+				},
+				secretKey
+			);
+			req.log.info(`User logged in successfully`);
+			res.status(200).send(token);
+		} else {
+			req.log.warn(`Login failed: incorrect password`);
+			res.status(401).send("Username or password wrong");
+		}
+	} else {
+		req.log.warn(`Login failed: didn't find username: ${username}`);
+		res.status(401).send("Username or password wrong");
+	}
 };
 
 const verifyToken = async (req, res) => {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-        req.log.warn("Unauthorized attempt to verify token");
-        return res.sendStatus(401);
-    }
-    const token = authHeader.split(" ")[1];
-    if (!token) {
-        req.log.warn("Unauthorized attempt to verify token");
-        return res.sendStatus(401);
-    }
-    jwt.verify(token, secretKey, async (err) => {
-        if (err) {
-            req.log.warn("Token verification failed");
-            return res.sendStatus(401);
-        }
+	const authHeader = req.headers["authorization"];
+	if (!authHeader) {
+		req.log.warn("Unauthorized attempt to verify token");
+		return res.sendStatus(401);
+	}
+	const token = authHeader.split(" ")[1];
+	if (!token) {
+		req.log.warn("Unauthorized attempt to verify token");
+		return res.sendStatus(401);
+	}
+	jwt.verify(token, secretKey, async (err) => {
+		if (err) {
+			req.log.warn("Token verification failed");
+			return res.sendStatus(401);
+		}
 
-        req.log.info(`Successfully verified token.`);
-        return res.status(200);
-    });
+		req.log.info(`Successfully verified token.`);
+		return res.status(200);
+	});
 };
 
 module.exports = { initializeAPI };
